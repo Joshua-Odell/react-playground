6c2fb9f616151d4b3deec2677fa09e12201e7824
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-27 09:50:45 -0500

    changed color

--- src/composition/Tooltip.js
+++ src/composition/Tooltip.js
@@ -4,7 +4,7 @@ import './Tooltip.css';
 function Tooltip(props){
     return (
         <span className= 'Tooltip'>
-            <span className='Tooltip-content'>
+            <span className='Tooltip-content' style={{ color: props.color}}>
                 {props.children}
             </span>
             <div className='Tooltip-message'>
@@ -14,4 +14,23 @@ function Tooltip(props){
     )
 }
 
-export default Tooltip;
+class TooltipCLass extends React.Component {
+    render(){
+        console.log('Using a class component!')
+        console.log(this.props)
+        return (
+        <span className = 'Tooltip'>
+            <span
+            className='Tooltip-content'
+            style={{ color: this.props.color}}>
+            {this.props.children}
+            </span>
+            <div className = 'Tooltip-message'>
+                {this.props.message}
+            </div>
+        </span>
+        )
+    }
+}
+
+export default TooltipClass;
