4d7674dd69d7981f6073405f5f35086246607b77
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-29 10:37:36 -0500

    fixed errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM, { unmountComponentAtNode } from 'react-dom';
 import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+ 
+//smoke test
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<app />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
